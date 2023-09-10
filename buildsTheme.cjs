d5f7161460a3b486a01fed7109192016844d4bfe
andysobol <a.sobolev@ciprosoft.com>
2023-09-10 18:25:32 +0300

    1

--- buildsTheme.cjs
+++ buildsTheme.cjs
@@ -12,11 +12,14 @@ registerTransforms(StyleDictionary, {
 // example value transform, which just returns the token as is
 StyleDictionary.registerTransform({
   type: 'value',
-  name: 'myCustomTransform',
-  matcher: (token) => {},
-  transformer: (token) => {
-    return token; // <-- transform as needed
-  },
+  name: 'colors/hex8flutter',
+  matcher: prop => {
+		return prop.attributes.category === 'colors'
+	},
+	transformer: prop => {
+		var str = Color(prop.value).toHex8().toUpperCase();
+		return `Color(0x${str.slice(6)}${str.slice(0, 6)})`;
+	},
 });
 
 // format helpers from style-dictionary
@@ -30,13 +33,28 @@ async function run() {
     platforms: {
 			flutter: {
 				buildPath: 'build/flutter/',
-				transformGroup: 'flutter',
+				transforms: ["attribute/cti", "name/cti/camel", "color/hex8flutter", "size/flutter/remToDouble", "content/flutter/literal", "asset/flutter/literal", "font/flutter/literal", "colors/hex8flutter"],
 				files: [
 					{
 						destination: `vars-${name}.dart`,
 						format: 'flutter/class.dart',
 					},
 				],
+			},
+			flutterSeparate:{
+				buildPath: 'build/flutterSeparate/',
+				transforms: ["attribute/cti", "name/cti/camel", "color/hex8flutter", "size/flutter/remToDouble", "content/flutter/literal", "asset/flutter/literal", "font/flutter/literal", "colors/hex8flutter"],
+				files:[{
+					destination: `vars-${name}.dart`,
+					format: 'flutter/class.dart',
+					className:'StyleDictionaryColor',
+					type: 'color',
+					filter:{
+						attributes:{
+							category:'colors'
+						},
+					},
+				}],
 			},
     },
   }));
